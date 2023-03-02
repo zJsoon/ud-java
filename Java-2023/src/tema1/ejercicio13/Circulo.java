ff16ce4ea5b2cfc4662543e583d64b2152040b7c
zJson <85413484+zJsoon@users.noreply.github.com>
2023-03-02 08:31:39 +0100

    Add files via upload

---
+++ Java-2023/src/tema1/ejercicio13/Circulo.java
@@ -0,0 +1,15 @@
+package tema1.ejercicio13;
+
+import java.awt.Color;
+
+public class Circulo {
+	
+	private static float grosor = 1.0f; // Grosor del círculo, en píxeles.
+	private int radioEnPixels; // Radio del circulo en píxeles.
+	private int xCentro; // Coordenadas X del circulo.
+	private int yCentro; // Coordenadas Y del circulo.
+	private Color colorBorde;
+	private Color colorCentro;
+	
+	
+}
